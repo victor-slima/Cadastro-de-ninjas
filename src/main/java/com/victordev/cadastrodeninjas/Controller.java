634c5c65dff14fff457bf6aaeef69d5f96dd07a0
victor-slima <victorslima.dev@gmail.com>
2025-12-04 08:53:53 -0300

    Criado a controller.

---
+++ src/main/java/com/victordev/cadastrodeninjas/Controller.java
@@ -0,0 +1,16 @@
+package com.victordev.cadastrodeninjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Bem vindo ao nosso sistema seu fi de rapariga";
+    };
+}
